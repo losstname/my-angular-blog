8d1749db12fba125e30e108e77a7025fde8e1f74
Umar <umrwhk@gmail.com>
2025-05-21 17:44:59 +0800

    fix: post filter

--- src/app/shared/components/post-filter/post-filter.component.ts
+++ src/app/shared/components/post-filter/post-filter.component.ts
@@ -2,6 +2,8 @@ import {Component, EventEmitter, Output} from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {FormsModule} from "@angular/forms";
 import {MatInput} from "@angular/material/input";
+import {MatDivider} from "@angular/material/divider";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-post-filter',
@@ -10,7 +12,9 @@ import {MatInput} from "@angular/material/input";
     MatFormField,
     FormsModule,
     MatInput,
-    MatLabel
+    MatLabel,
+    MatDivider,
+    MatIcon
   ],
   templateUrl: './post-filter.component.html',
   styleUrl: './post-filter.component.scss'
